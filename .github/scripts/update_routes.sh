70f72dc2fd953aaa82cf3e89660fc5d091f99969
mtrshuvo <mtrshuvo@ymail.com>
2025-09-01 22:44:28 +0600

    fixing

--- .github/scripts/update_routes.sh
+++ .github/scripts/update_routes.sh
@@ -1,69 +1,3 @@
-# #!/usr/bin/env bash
-# set -euo pipefail
-
-# CONFIG_FILE=".github/config/lambdas.json"
-
-# for route in $(jq -r '.routes | keys[]' "$CONFIG_FILE"); do
-#   RESOURCE_ID=$(aws apigateway get-resources --rest-api-id $REST_API_ID --query "items[?path=='$route'].id" --output text)
-#   METHODS=$(jq -r ".routes[\"$route\"].methods | keys[]" "$CONFIG_FILE")
-
-#   for method in $METHODS; do
-#     TYPE=$(jq -r ".routes[\"$route\"].methods[\"$method\"].type" "$CONFIG_FILE")
-#     case "$TYPE" in
-#       lambda)
-#         LAMBDA_NAME=$(jq -r ".routes[\"$route\"].methods[\"$method\"].lambda_name" "$CONFIG_FILE")
-#         LAMBDA_ARN="arn:aws:apigateway:$REGION:lambda:path/2015-03-31/functions/arn:aws:lambda:$REGION:300080618312:function:$LAMBDA_NAME:$ENV/invocations"
-        
-#         SAFE_ROUTE=$(echo "$route" | sed 's|/|-|g' | sed 's|^-||')  # replace / with - and remove leading -
-#         STATEMENT_ID="apigateway-$ENV-$SAFE_ROUTE-$method"
-
-#         aws lambda add-permission \
-#           --function-name "$LAMBDA_NAME:$ENV" \
-#           --statement-id "$STATEMENT_ID" \
-#           --action lambda:InvokeFunction \
-#           --principal apigateway.amazonaws.com \
-#           --source-arn "arn:aws:execute-api:$REGION:300080618312:$REST_API_ID/*/$method$route" || true
-
-
-        
-#         aws apigateway put-integration \
-#           --rest-api-id $REST_API_ID \
-#           --resource-id $RESOURCE_ID \
-#           --http-method $method \
-#           --type AWS_PROXY \
-#           --integration-http-method POST \
-#           --uri $LAMBDA_ARN
-#         ;;
-#       cors)
-#         ORIGIN=$(jq -r ".routes[\"$route\"].methods[\"$method\"].origin" "$CONFIG_FILE")
-#         aws apigateway put-integration-response \
-#           --rest-api-id $REST_API_ID \
-#           --resource-id $RESOURCE_ID \
-#           --http-method OPTIONS \
-#           --status-code 200 \
-#           --response-parameters "{
-#             \"method.response.header.Access-Control-Allow-Headers\":\"'Content-Type,Authorization'\",
-#             \"method.response.header.Access-Control-Allow-Methods\":\"'GET,OPTIONS'\",
-#             \"method.response.header.Access-Control-Allow-Origin\":\"'$ORIGIN'\"
-#           }"
-#         ;;
-#     esac
-#   done
-# done
-
-# aws apigateway create-deployment --rest-api-id $REST_API_ID --stage-name $ENV --description "Updated all routes for $ENV"
-
-
-
-
-
-
-
-
-
-
-
-
 #!/usr/bin/env bash
 set -euo pipefail
 
@@ -116,7 +50,6 @@ for route in $(jq -r '.routes | keys[]' "$CONFIG_FILE"); do
         fi
         ;;
       cors)
-        ORIGIN=$(jq -r ".routes[\"$route\"].methods[\"$method\"].origin" "$CONFIG_FILE")
         aws apigateway put-integration-response \
           --rest-api-id $REST_API_ID \
           --resource-id $RESOURCE_ID \
@@ -125,7 +58,7 @@ for route in $(jq -r '.routes | keys[]' "$CONFIG_FILE"); do
           --response-parameters "{
             \"method.response.header.Access-Control-Allow-Headers\":\"'Content-Type,Authorization'\",
             \"method.response.header.Access-Control-Allow-Methods\":\"'GET,OPTIONS'\",
-            \"method.response.header.Access-Control-Allow-Origin\":\"'$ORIGIN'\"
+            \"method.response.header.Access-Control-Allow-Origin\":\"'$OPTIONS_CORS_DOMAIN'\"
           }"
         ;;
       *)
